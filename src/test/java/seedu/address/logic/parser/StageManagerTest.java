d1cd076688aa681d5725911b2e94042d5cc0e556
Gallen <90502827+gongg21@users.noreply.github.com>
2023-11-10 02:25:58 +0800

    Fix EditCommandTest

--- src/test/java/seedu/address/logic/parser/StageManagerTest.java
+++ src/test/java/seedu/address/logic/parser/StageManagerTest.java
@@ -30,11 +30,13 @@ class StageManagerTest {
 
     @Test
     void testGetStage() {
+        stageManager.setHomeStage();
         assertEquals(Stages.HOME, stageManager.getStage(), "Initial stage should be HOME");
     }
 
     @Test
     void testGetSelectedCourse() {
+        stageManager.setHomeStage();
         assertNull(stageManager.getSelectedCourse(), "Selected course should be null initially");
     }
 
@@ -68,6 +70,7 @@ class StageManagerTest {
 
     @Test
     void testIsSelectedCourseNull() {
+        stageManager.setHomeStage();
         assertTrue(stageManager.isSelectedCourseNull(), "Selected course should be null initially");
         Course course = new CourseBuilder().build();
         stageManager.setCourseStage(course);
